ec28b842aeea6065a4ab3c505b53a37bb4ec166b
jrafaaael <jrmd070102@gmail.com>
2023-05-17 11:54:22 -0400

    chore: move layout related code to `MainLayout` instead of per page

--- resources/views/components/layouts/main-layout.blade.php
+++ resources/views/components/layouts/main-layout.blade.php
@@ -3,12 +3,7 @@
         [
             'label' => 'dashboard',
             'icon' => 'icons.home',
-            'childrens' => [
-                ['label' => 'Nested', 'icon' => 'icons.home'],
-                ['label' => 'Nested', 'icon' => 'icons.home'],
-                ['label' => 'Nested', 'icon' => 'icons.home'],
-                ['label' => 'Nested', 'icon' => 'icons.home']
-            ]
+            'childrens' => [['label' => 'Nested', 'icon' => 'icons.home'], ['label' => 'Nested', 'icon' => 'icons.home'], ['label' => 'Nested', 'icon' => 'icons.home'], ['label' => 'Nested', 'icon' => 'icons.home']],
         ],
         ['label' => 'dashboard2', 'icon' => 'icons.home'],
         ['label' => 'dashboard3', 'icon' => 'icons.home'],
@@ -21,41 +16,45 @@
         [
             'label' => 'dashboard10',
             'icon' => 'icons.home',
-            'childrens' => [
-                ['label' => 'Nested', 'icon' => 'icons.home'],
-                ['label' => 'Nested', 'icon' => 'icons.home'],
-                ['label' => 'Nested', 'icon' => 'icons.home'],
-                ['label' => 'Nested', 'icon' => 'icons.home']
-            ]
-        ], 
+            'childrens' => [['label' => 'Nested', 'icon' => 'icons.home'], ['label' => 'Nested', 'icon' => 'icons.home'], ['label' => 'Nested', 'icon' => 'icons.home'], ['label' => 'Nested', 'icon' => 'icons.home']],
+        ],
     ];
 @endphp
 
-<main class="flex relative">
-    <aside
-        class="w-full h-screen p-4 bg-white hidden fixed inset-0 [&.open]:block lg:w-80 lg:bg-transparent lg:border-r lg:border-solid lg:border-gray-300 lg:block lg:relative">
-        <x-tree :links="$links" />
-    </aside>
-    <div>
-        <header>
-            <h1>¡Bienvenido de vuelta!</h1>
-        </header>
-        {{ $slot }}
-    </div>
-</main>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-@push('tree')
-    <script>
-        const triggers = document.querySelectorAll("button[data-trigger]");
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="preload" href="{{ asset('fonts/Inter-Regular.ttf') }}" as="font" type="font/ttf" crossorigin>
 
-        triggers.forEach((element) => {
-            element.addEventListener("click", () => {
-                const nestedNavValue = element.getAttribute("data-trigger");
-                const nestedNav = document.querySelector(
-                    `[data-nested="${nestedNavValue}"]`
-                );
-                nestedNav?.classList.toggle("open");
-            });
-        });
-    </script>
-@endpush
+    <style>
+        @font-face {
+            font-family: "Inter-Regular";
+            src: url("{{ asset('fonts/Inter-Regular.ttf') }}");
+            font-style: normal;
+            font-weight: 400;
+        }
+    </style>
+</head>
+
+<body class="antialiased text-neutral-800">
+    <main class="flex relative">
+        <aside
+            class="w-full h-screen p-4 bg-white hidden fixed inset-0 [&.open]:block lg:w-80 lg:bg-transparent lg:border-r lg:border-solid lg:border-gray-300 lg:block lg:relative">
+            <x-tree :links="$links" />
+        </aside>
+        <div>
+            <header>
+                <h1>¡Bienvenido de vuelta!</h1>
+            </header>
+            {{ $slot }}
+        </div>
+    </main>
+    @stack('tree')
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,26 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Laravel</title>
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-  <link rel="preload" href="{{ asset('fonts/Inter-Regular.ttf') }}" as="font" type="font/ttf" crossorigin>
-
-  <style>
-    @font-face {
-      font-family: "Inter-Regular";
-      src: url("{{ asset('fonts/Inter-Regular.ttf') }}");
-      font-style: normal;
-      font-weight: 400;
-    }
-  </style>
-</head>
-
-<body class="antialiased text-neutral-800">
-  <x-layouts.main-layout />
-  @stack('tree')
-</body>
-
-</html>
+<x-layouts.main-layout>
+    <section>
+        <h1>Hola!</h1>
+    </section>
+</x-layouts.main-layout>
