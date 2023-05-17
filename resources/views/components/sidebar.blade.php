8ffa685a38373522f7d0a1c0e8a75ef56d98c75b
jrafaaael <jrmd070102@gmail.com>
2023-05-17 12:50:13 -0400

    feat: add `Sidebar` component

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,34 @@
+<aside
+    class="w-full min-h-screen p-4 bg-white hidden fixed inset-0 overflow-y-auto [&::-webkit-scrollbar]:hidden [-ms-overflow-style:'none'] [scrollbar-width:'none'] [&.open]:block lg:w-80 lg:bg-transparent lg:border-r lg:border-solid lg:border-gray-300 lg:block"
+    data-sidebar="{{ $name }}">
+    {{ $slot }}
+</aside>
+
+@once
+    @push('aside')
+        <script defer>
+            const sidebarOpenBtns = document.querySelectorAll("button[data-sidebar-open]");
+            const sidebarCloseBtns = document.querySelectorAll("button[data-sidebar-close]");
+
+            sidebarOpenBtns.forEach((element) => {
+                element.addEventListener("click", () => {
+                    const sidebarToOpen = element.getAttribute("data-sidebar-open");
+                    const sidebar = document.querySelector(
+                        `[data-sidebar="${sidebarToOpen}"]`
+                    );
+                    sidebar?.classList.add("open");
+                });
+            });
+
+            sidebarCloseBtns.forEach((element) => {
+                element.addEventListener("click", () => {
+                    const sidebarToClose = element.getAttribute("data-sidebar-close");
+                    const sidebar = document.querySelector(
+                        `[data-sidebar="${sidebarToClose}"]`
+                    );
+                    sidebar?.classList.remove("open");
+                })
+            })
+        </script>
+    @endpush
+@endonce
