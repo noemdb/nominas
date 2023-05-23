d5607efbc41b2387b731754a06783850613b8667
jrafaaael <jrmd070102@gmail.com>
2023-05-23 10:02:23 -0400

    fix: add missing routes related to `institutions`

---
+++ resources/views/institutions/rols/index.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.main-layout>
+    <main>
+        <livewire:institution.rol.index-component />
+    </main>
+</x-layouts.main-layout>
---
+++ resources/views/institutions/specials/index.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.main-layout>
+    <main>
+        <livewire:institution.special.index-component />
+    </main>
+</x-layouts.main-layout>
