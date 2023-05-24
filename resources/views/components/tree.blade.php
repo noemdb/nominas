3b02ab3e5cd1c5043c65da7a7e13775691babc43
jrafaaael <jrmd070102@gmail.com>
2023-05-24 10:19:36 -0400

    feat: keep open trees on navigation

--- resources/views/components/tree.blade.php
+++ resources/views/components/tree.blade.php
@@ -9,7 +9,8 @@
                         <div class="w-5 h-5">
                             <x-dynamic-component :component="$link['icon']" />
                         </div>
-                        <span class="mr-auto text-sm text-left lowercase first-letter:uppercase">{{ $link['label'] }}</span>
+                        <span
+                            class="mr-auto text-sm text-left lowercase first-letter:uppercase">{{ $link['label'] }}</span>
                         <div class="w-3 h-3">
                             <x-icons.arrow-down />
                         </div>
@@ -18,7 +19,7 @@
                         <x-tree :links="$link['childrens']" :isNested="true" />
                     </div>
                 @else
-                    <a href={{$link['route']}}
+                    <a href={{ $link['route'] }}
                         class="px-4 py-3 rounded-lg flex items-center gap-4 hover:bg-green-200/40 hover:text-green-900">
                         <div class="w-5 h-5 relative flex items-center justify-center">
                             @if ($isNested)
@@ -38,21 +39,42 @@
 @once
     @push('tree')
         <script defer>
+            const TREE_IDENTIFIER = "openTrees";
+            // Get all button that can open a nested tree
             const treeTriggers = document.querySelectorAll("button[data-tree-trigger]");
-
-            //const routeCurrent = "{{ url()->current() }}"; console.log(routeCurrent);
+            // Get all saved opened trees
+            let openTrees = JSON.parse(sessionStorage.getItem(TREE_IDENTIFIER)) ?? [];
 
             treeTriggers.forEach((element) => {
                 element.addEventListener("click", () => {
-                    const treeToTrigger = element.getAttribute("data-tree-trigger");
-                    const tree = document.querySelector(
-                        `[data-nested-tree="${treeToTrigger}"]`
-                    );
+                    const treeIdentifier = element.getAttribute("data-tree-trigger");
+                    const treeSelector = `[data-nested-tree="${treeIdentifier}"]`
+                    const tree = document.querySelector(treeSelector);
 
+                    // Check if nested tree is already open
+                    if (tree?.classList.contains("open")) {
+                        // if so, then remove it from the saved list
+                        openTrees = openTrees?.filter((treeName) => treeName !== treeIdentifier);
+                    } else {
+                        // else add it to the saved list
+                        openTrees?.push(treeIdentifier);
+                    }
+
+                    // Here is saved the new list
+                    sessionStorage.setItem(TREE_IDENTIFIER, JSON.stringify(openTrees));
                     tree?.classList.toggle("open");
                 });
             });
 
+            document.addEventListener("DOMContentLoaded", () => {
+                openTrees.forEach((value) => {
+                    const treeSelector = `[data-nested-tree="${value}"]`;
+                    const tree = document.querySelector(treeSelector);
+
+                    // Add class "open" to tree. Only to elements inside saved list (openTrees)
+                    tree?.classList.add("open");
+                });
+            });
         </script>
     @endpush
 @endonce
