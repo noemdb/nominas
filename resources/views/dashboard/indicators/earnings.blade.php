5281802b6e5151d5dd3dce258635bd11ae1919f5
jrafaaael <jrmd070102@gmail.com>
2023-06-09 10:43:56 -0400

    style: formatting

--- resources/views/dashboard/indicators/earnings.blade.php
+++ resources/views/dashboard/indicators/earnings.blade.php
@@ -1,63 +1,63 @@
 <h2 class="mb-4 text-green-700 text-2xl font-bold">Resumen general</h2>
-    <div class="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
-        <article class="p-4 border border-neutral-200 rounded-lg">
-            <h6 class="mb-2 text-neutral-500">Costo total de la nómina</h6>
-            <p class="mb-4 text-green-700 text-xl font-bold">$50.000,23</p>
-            <div class="flex items-center">
-                <p>
-                    <span class="w-5 h-5 text-green-700 inline-block align-text-bottom">
-                        <x-icons.trending />
-                    </span>
-                    <span class="text-green-700 font-bold">
-                        2.5%
-                    </span>
-                    vs. hace 6 meses
-                </p>
-            </div>
-        </article>
-        <article class="p-4 border border-neutral-200 rounded-lg">
-            <h6 class="mb-2 text-neutral-500">Número de empleados activos</h6>
-            <p class="mb-4 text-red-700 text-xl font-bold">80</p>
-            <div class="flex items-center">
-                <p>
-                    <span class="w-5 h-5 text-red-700 inline-block align-text-bottom rotate-90">
-                        <x-icons.trending />
-                    </span>
-                    <span class="text-red-700 font-bold">
-                        -2.5%
-                    </span>
-                    vs. hace 6 meses
-                </p>
-            </div>
-        </article>
-        <article class="p-4 border border-neutral-200 rounded-lg">
-            <h6 class="mb-2 text-neutral-500">Tasa de absentismo</h6>
-            <p class="mb-4 text-green-700 text-xl font-bold">12%</p>
-            <div class="flex items-center">
-                <p>
-                    <span class="w-5 h-5 text-green-700 inline-block align-text-bottom">
-                        <x-icons.trending />
-                    </span>
-                    <span class="text-green-700 font-bold">
-                        2.5%
-                    </span>
-                    vs. hace 6 meses
-                </p>
-            </div>
-        </article>
-        <article class="p-4 border border-neutral-200 rounded-lg">
-            <h6 class="mb-2 text-neutral-500">Eficiencia del proceso de nómina</h6>
-            <p class="mb-4 text-green-700 text-xl font-bold">99.8%</p>
-            <div class="flex items-center">
-                <p>
-                    <span class="w-5 h-5 text-green-700 inline-block align-text-bottom">
-                        <x-icons.trending />
-                    </span>
-                    <span class="text-green-700 font-bold">
-                        1.5%
-                    </span>
-                    vs. hace 6 meses
-                </p>
-            </div>
-        </article>
-    </div>
+<div class="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+    <article class="p-4 border border-neutral-200 rounded-lg">
+        <h6 class="mb-2 text-neutral-500">Costo total de la nómina</h6>
+        <p class="mb-4 text-green-700 text-xl font-bold">$50.000,23</p>
+        <div class="flex items-center">
+            <p>
+                <span class="w-5 h-5 text-green-700 inline-block align-text-bottom">
+                    <x-icons.trending />
+                </span>
+                <span class="text-green-700 font-bold">
+                    2.5%
+                </span>
+                vs. hace 6 meses
+            </p>
+        </div>
+    </article>
+    <article class="p-4 border border-neutral-200 rounded-lg">
+        <h6 class="mb-2 text-neutral-500">Número de empleados activos</h6>
+        <p class="mb-4 text-red-700 text-xl font-bold">80</p>
+        <div class="flex items-center">
+            <p>
+                <span class="w-5 h-5 text-red-700 inline-block align-text-bottom rotate-90">
+                    <x-icons.trending />
+                </span>
+                <span class="text-red-700 font-bold">
+                    -2.5%
+                </span>
+                vs. hace 6 meses
+            </p>
+        </div>
+    </article>
+    <article class="p-4 border border-neutral-200 rounded-lg">
+        <h6 class="mb-2 text-neutral-500">Tasa de absentismo</h6>
+        <p class="mb-4 text-green-700 text-xl font-bold">12%</p>
+        <div class="flex items-center">
+            <p>
+                <span class="w-5 h-5 text-green-700 inline-block align-text-bottom">
+                    <x-icons.trending />
+                </span>
+                <span class="text-green-700 font-bold">
+                    2.5%
+                </span>
+                vs. hace 6 meses
+            </p>
+        </div>
+    </article>
+    <article class="p-4 border border-neutral-200 rounded-lg">
+        <h6 class="mb-2 text-neutral-500">Eficiencia del proceso de nómina</h6>
+        <p class="mb-4 text-green-700 text-xl font-bold">99.8%</p>
+        <div class="flex items-center">
+            <p>
+                <span class="w-5 h-5 text-green-700 inline-block align-text-bottom">
+                    <x-icons.trending />
+                </span>
+                <span class="text-green-700 font-bold">
+                    1.5%
+                </span>
+                vs. hace 6 meses
+            </p>
+        </div>
+    </article>
+</div>
