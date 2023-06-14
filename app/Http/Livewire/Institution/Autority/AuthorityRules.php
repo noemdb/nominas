a101170cc93b99a37eca9299ae78540f3cfcb05b
jrafaaael <jrmd070102@gmail.com>
2023-06-14 13:21:47 -0400

    feat: add validation rules

---
+++ app/Http/Livewire/Institution/Autority/AuthorityRules.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Livewire\Institution\Autority;
+
+trait AuthorityRules
+{
+    protected $rules = [
+        'authority.institution_id' => 'required|string',
+        'authority.name' => 'required|string',
+        'authority.position' => 'required|string',
+        'authority.email' => 'required|string|email',
+        'authority.phone_number' => 'required|string',
+        'authority.address' => 'required|string',
+        'authority.profile_professional' => 'required|string',
+        'authority.finicial' => 'required|date',
+        'authority.ffinal' => 'required|date',
+        'authority.photo' => 'nullable|string',
+    ];
+
+    protected function validationAttributes()
+    {
+        return [
+            'authority.institution_id' => $this->list_comment['institution_id'],
+            'authority.name' => $this->list_comment['name'],
+            'authority.position' => $this->list_comment['position'],
+            'authority.email' => $this->list_comment['email'],
+            'authority.phone_number' => $this->list_comment['phone_number'],
+            'authority.address' => $this->list_comment['address'],
+            'authority.profile_professional' => $this->list_comment['profile_professional'],
+            'authority.finicial' => $this->list_comment['finicial'],
+            'authority.ffinal' => $this->list_comment['ffinal'],
+            'authority.photo' => $this->list_comment['photo'],
+        ];
+    }
+}
--- app/Http/Livewire/Institution/Autority/IndexComponent.php
+++ app/Http/Livewire/Institution/Autority/IndexComponent.php
@@ -4,15 +4,21 @@ namespace App\Http\Livewire\Institution\Autority;
 // touch 'app/Http/Livewire/Institution/AutorityRules.php'
 // touch 'app/Http/Livewire/Common/WithSortingTrait.php'
 
+use App\Http\Livewire\Institution\Autority\AuthorityRules;
 use App\Models\Institution\Authority;
 use Livewire\Component;
 
 class IndexComponent extends Component
 {
+    use AuthorityRules;
+
+    public Authority $authority;
     public $list_comment;
     public $showModal = false;
 
-    public function mount() {
+    public function mount()
+    {
+        $this->authority = new Authority;
         $this->list_comment = Authority::COLUMN_COMMENTS;
     }
 
@@ -20,5 +26,12 @@ class IndexComponent extends Component
     {
         $authorities = Authority::all();
         return view('livewire.institution.autority.index-component', ['authorities' => $authorities]);
+    }
+
+    public function save()
+    {
+        $this->validate();
+        $this->authority->save();
+        $this->authority = new Authority;
     }
 }
