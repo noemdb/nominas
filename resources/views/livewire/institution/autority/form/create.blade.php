f52b81e2131b5a4681638ed33ea7f02b2e1b9e51
jrafaaael <jrmd070102@gmail.com>
2023-06-14 13:05:35 -0400

    fix: change model name

--- resources/views/livewire/institution/autority/form/create.blade.php
+++ resources/views/livewire/institution/autority/form/create.blade.php
@@ -2,7 +2,7 @@
     <div class="py-4">
         @php
             $name = 'name';
-            $model = 'institution.' . $name;
+            $model = 'authority.' . $name;
             $comment = $list_comment[$name];
         @endphp
         <x-input wire:model.defer="{{ $model }}" label="{{ $comment }}" placeholder="{{ $comment }}"
@@ -12,7 +12,7 @@
     <div class="py-4">
         @php
             $name = 'position';
-            $model = 'institution.' . $name;
+            $model = 'authority.' . $name;
             $comment = $list_comment[$name];
         @endphp
         <x-input wire:model.defer="{{ $model }}" label="{{ $comment }}" placeholder="{{ $comment }}" />
@@ -21,7 +21,7 @@
     <div class="py-4">
         @php
             $name = 'email';
-            $model = 'institution.' . $name;
+            $model = 'authority.' . $name;
             $comment = $list_comment[$name];
         @endphp
         <x-input wire:model.defer="{{ $model }}" label="{{ $comment }}" placeholder="{{ $comment }}"
@@ -31,7 +31,7 @@
     <div class="py-4">
         @php
             $name = 'phone_number';
-            $model = 'institution.' . $name;
+            $model = 'authority.' . $name;
             $comment = $list_comment[$name];
         @endphp
         <x-inputs.maskable wire:model.defer="{{ $model }}" label="{{ $comment }}" mask="(###) ###-####"
@@ -41,7 +41,7 @@
     <div class="py-4">
         @php
             $name = 'address';
-            $model = 'institution.' . $name;
+            $model = 'authority.' . $name;
             $comment = $list_comment[$name];
         @endphp
         <x-textarea wire:model.defer="{{ $model }}" label="{{ $comment }}"
@@ -50,7 +50,7 @@
     <div class="py-4">
         @php
             $name = 'profile_professional';
-            $model = 'institution.' . $name;
+            $model = 'authority.' . $name;
             $comment = $list_comment[$name];
         @endphp
         <x-input wire:model.defer="{{ $model }}" label="{{ $comment }}"
@@ -60,7 +60,7 @@
     <div class="py-4">
         @php
             $name = 'finicial';
-            $model = 'institution.' . $name;
+            $model = 'authority.' . $name;
             $comment = $list_comment[$name];
         @endphp
         <x-datetime-picker label="{{ $comment }}" placeholder="{{ $comment }}"
@@ -70,7 +70,7 @@
     <div class="py-4">
         @php
             $name = 'ffinal';
-            $model = 'institution.' . $name;
+            $model = 'authority.' . $name;
             $comment = $list_comment[$name];
         @endphp
         <x-datetime-picker label="{{ $comment }}" placeholder="{{ $comment }}"
