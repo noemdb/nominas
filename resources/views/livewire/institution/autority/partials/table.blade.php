9742d86ceee38ca1f020ca76b321dd26856f0d1f
jrafaaael <jrmd070102@gmail.com>
2023-06-15 15:39:37 -0400

    style: formating

--- resources/views/livewire/institution/autority/partials/table.blade.php
+++ resources/views/livewire/institution/autority/partials/table.blade.php
@@ -1,14 +1,20 @@
 <div class="mb-4 flex justify-between">
     <div class="px-2 w-3/4">
-        @php $name = 'search'; $model = 'institution.'.$name; @endphp
+        @php
+            $name = 'search';
+            $model = 'institution.' . $name;
+        @endphp
         <div class="flex justify-between">
-            <div wire:loading class="text-black font-semibold fixed	 bottom-0 right-0 z-10 bg-white rounded border shadow mr-2 mb-2 dark:text-gray-100"> Cargando... </div>
+            <div wire:loading
+                class="text-black font-semibold fixed	 bottom-0 right-0 z-10 bg-white rounded border shadow mr-2 mb-2 dark:text-gray-100">
+                Cargando... </div>
         </div>
         @php $label = "Nombre, tipo, dirección o teléfono" @endphp
-        <x-input wire:model.debounce.500ms="{{$name}}" icon="search" label="{{$label}}" placeholder="{{$label}}" >
+        <x-input wire:model.debounce.500ms="{{ $name }}" icon="search" label="{{ $label }}"
+            placeholder="{{ $label }}">
             <x-slot name="append">
                 <div class="absolute inset-y-0 right-0 flex items-center p-0.5 text-gray-600">
-                    <x-icon name="x" class="w-4 h-4" wire:click="cleanSearch()"/>
+                    <x-icon name="x" class="w-4 h-4" wire:click="cleanSearch()" />
                 </div>
             </x-slot>
         </x-input>
@@ -26,36 +32,39 @@
                 <th class="px-4 py-2">
                     <div class="flex justify-between">
                         @php $name = 'name' @endphp
-                        <div>{{$list_comment[$name] ?? ''}}</div>
-                        @if($authorities->isNotEmpty())
-                            <x-elements.crud.sort-by field="{{$name}}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
+                        <div>{{ $list_comment[$name] ?? '' }}</div>
+                        @if ($authorities->isNotEmpty())
+                            <x-elements.crud.sort-by field="{{ $name }}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
                         @endif
                     </div>
                 </th>
                 <th class="px-4 py-2">
                     <div class="flex justify-between">
                         @php $name = 'position' @endphp
-                        <div>{{$list_comment[$name] ?? ''}}</div>
-                        @if($authorities->isNotEmpty())
-                            <x-elements.crud.sort-by field="{{$name}}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
+                        <div>{{ $list_comment[$name] ?? '' }}</div>
+                        @if ($authorities->isNotEmpty())
+                            <x-elements.crud.sort-by field="{{ $name }}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
                         @endif
                     </div>
                 </th>
                 <th class="px-4 py-2">
                     <div class="flex justify-between">
                         @php $name = 'profile_professional' @endphp
-                        <div>{{$list_comment[$name] ?? ''}}</div>
-                        @if($authorities->isNotEmpty())
-                            <x-elements.crud.sort-by field="{{$name}}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
+                        <div>{{ $list_comment[$name] ?? '' }}</div>
+                        @if ($authorities->isNotEmpty())
+                            <x-elements.crud.sort-by field="{{ $name }}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
                         @endif
                     </div>
                 </th>
                 <th class="px-4 py-2">
                     <div class="flex justify-between">
-                        @php $name = 'finicial'; $name2 = 'ffinal' @endphp
-                        <div>{{$list_comment[$name] . ' - ' . $list_comment[$name2] ?? ''}}</div>
-                        @if($authorities->isNotEmpty())
-                            <x-elements.crud.sort-by field="{{$name}}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
+                        @php
+                            $name = 'finicial';
+                            $name2 = 'ffinal';
+                        @endphp
+                        <div>{{ $list_comment[$name] . ' - ' . $list_comment[$name2] ?? '' }}</div>
+                        @if ($authorities->isNotEmpty())
+                            <x-elements.crud.sort-by field="{{ $name }}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
                         @endif
                     </div>
                 </th>
@@ -65,15 +74,17 @@
         <tbody>
             @forelse ($authorities as $item)
                 <tr class="border-t border-gray-200">
-                    <td class="px-4 py-2">{{$loop->iteration}}</td>
-                    <td class="px-4 py-2">{{$item->name}}</td>
-                    <td class="px-4 py-2">{{$item->position}}</td>
-                    <td class="px-4 py-2">{{$item->profile_professional}}</td>
-                    <td class="px-4 py-2">{{$item->finicial}} - {{$item->ffinal}}</td>
+                    <td class="px-4 py-2">{{ $loop->iteration }}</td>
+                    <td class="px-4 py-2">{{ $item->name }}</td>
+                    <td class="px-4 py-2">{{ $item->position }}</td>
+                    <td class="px-4 py-2">{{ $item->profile_professional }}</td>
+                    <td class="px-4 py-2">{{ $item->finicial }} - {{ $item->ffinal }}</td>
                     <td class="px-4 py-2">
                         <div class="flex">
-                            <x-button.circle wire:click="edit({{$item->id}})" primary icon="clipboard-list" class="mx-1"/>
-                            <x-button.circle wire:click="deleteQuestion({{$item->id}})" negative icon="x" class="mx-1"/>
+                            <x-button.circle wire:click="edit({{ $item->id }})" primary icon="clipboard-list"
+                                class="mx-1" />
+                            <x-button.circle wire:click="deleteQuestion({{ $item->id }})" negative icon="x"
+                                class="mx-1" />
                         </div>
                     </td>
                 </tr>
@@ -83,9 +94,7 @@
                         No hay datos
                     </td>
                 </tr>
-
             @endforelse
-
 
         </tbody>
     </table>
