9486f443256e32afd59916885790ed3618daf0ab
jrafaaael <jrmd070102@gmail.com>
2023-06-15 17:18:58 -0400

    feat: add delete method

--- app/Http/Livewire/Institution/Autority/IndexComponent.php
+++ app/Http/Livewire/Institution/Autority/IndexComponent.php
@@ -51,4 +51,56 @@ class IndexComponent extends Component
         $this->authority = Authority::findOrFail($id);
         $this->showModal = true;
     }
+
+    public function delete($id)
+    {
+        $authority = Authority::findOrFail($id);
+
+        $authority->delete();
+        $this->authority = new Authority;
+
+        $this->notification([
+            'title'       => 'Felicitaciones!!!',
+            'description' => 'Operación realizada',
+            'icon'        => 'success'
+        ]);
+    }
+
+    public function cancel()
+    {
+        $this->notification([
+            'title'       => 'Has cancelado!',
+            'description' => 'Ningún cambio realizado',
+            'icon'        => 'info'
+        ]);
+        $this->authority = new Authority;
+    }
+
+    public function deleteQuestion($id)
+    {
+        $this->authority = Authority::findOrFail($id);
+
+        if ($this->authority->status_delete) {
+            $this->notification()->confirm([
+                'title'       => 'Estas seguro que desea realizar esta operación?',
+                'description' => 'Eliminar registro?',
+                'icon'        => 'question',
+                'closeButton'        => true,
+                'accept'      => [
+                    'label'  => 'Aceptar',
+                    'method' => 'delete',
+                    'params' => $id,
+                ],
+                'reject' => [
+                    'label'  => 'No, cancelar',
+                    'method' => 'cancel',
+                ],
+            ]);
+        } else {
+            $this->notification()->error(
+                $title = 'Error!, Esta operación no se puede realizar.',
+                $description = 'Este registro tiene asociado otros, en caso de borrar, se pierde también los registro asociados.'
+            );
+        }
+    }
 }
