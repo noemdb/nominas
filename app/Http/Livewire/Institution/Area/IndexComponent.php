e2c9e71086c1be8559a3e97b2c7d963c8fb236c9
jrafaaael <jrmd070102@gmail.com>
2023-06-20 12:18:29 -0400

    feat: list areas

--- app/Http/Livewire/Institution/Area/IndexComponent.php
+++ app/Http/Livewire/Institution/Area/IndexComponent.php
@@ -2,12 +2,54 @@
 
 namespace App\Http\Livewire\Institution\Area;
 
+use App\Http\Livewire\Common\PaginateTrait;
+use App\Http\Livewire\Common\WithSortingTrait;
+use App\Models\Institution\Area;
 use Livewire\Component;
+use Livewire\WithPagination;
+use WireUi\Traits\Actions;
 
 class IndexComponent extends Component
 {
+
+    // use AuthorityRules;
+    use Actions;
+    use WithSortingTrait;
+    use WithPagination;
+    use PaginateTrait;
+
+    public Area $area;
+    public $list_comment;
+    public $list_institution;
+    public $showModal = false;
+    public $modeCreate = false;
+    public $modeEdit = false;
+    public $modeShow = false;
+
+    public function mount()
+    {
+        $this->area = new Area;
+        $this->list_institution = Area::list_institution();
+        $this->list_comment = Area::COLUMN_COMMENTS;
+    }
+
     public function render()
     {
-        return view('livewire.institution.area.index-component');
+        $search = $this->search;
+        $areas = Area::select('areas.*');
+
+        $areas = (!empty($search)) ? $areas->Where(
+            function ($query) use ($search) {
+                $query->orWhere('name', 'like', '%' . $search . '%');
+                $query->orWhere('position', 'like', '%' . $search . '%');
+                $query->orWhere('profile_professional', 'like', '%' . $search . '%');
+            }
+        )
+            : $areas;
+
+        $areas = ($this->sortBy && $this->sortDirection) ? $areas->orderBy($this->sortBy, $this->sortDirection) : $areas;
+
+        $areas = $areas->paginate($this->paginate);
+        return view('livewire.institution.area.index-component', ['areas' => $areas]);
     }
 }
--- resources/views/livewire/institution/area/index-component.blade.php
+++ resources/views/livewire/institution/area/index-component.blade.php
@@ -1,35 +1,11 @@
 <div>
-    <table class="min-w-full border-collapse">
-        <thead class="bg-gray-50">
-          <tr>
-            <th scope="col" class="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Nombre</th>
-            <th scope="col" class="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Apellido</th>
-            <th scope="col" class="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Correo electrónico</th>
-            <th scope="col" class="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Rol</th>
-            <th scope="col" class="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Acciones</th>
-          </tr>
-        </thead>
-        <tbody class="bg-white divide-y divide-gray-200">
-          <tr>
-            <td class="px-2 py-1 whitespace-nowrap">John</td>
-            <td class="px-2 py-1 whitespace-nowrap">Doe</td>
-            <td class="px-2 py-1 whitespace-nowrap">johndoe@example.com</td>
-            <td class="px-2 py-1 whitespace-nowrap">Administrador</td>
-            <td class="px-2 py-1 whitespace-nowrap">
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">Editar</button>
-              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">Eliminar</button>
-            </td>
-          </tr>
-          <tr>
-            <td class="px-2 py-1 whitespace-nowrap">Jane</td>
-            <td class="px-2 py-1 whitespace-nowrap">Smith</td>
-            <td class="px-2 py-1 whitespace-nowrap">janesmith@example.com</td>
-            <td class="px-2 py-1 whitespace-nowrap">Usuario</td>
-            <td class="px-2 py-1 whitespace-nowrap">
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">Editar</button>
-              <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">Eliminar</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+    <x-notifications />
+    {{-- @include('livewire.institution.autority.modals.main') --}}
+
+    <section class="mb-4 flex justify-between items-center">
+        <h2 class="text-2xl font-bold">Áreas registradas</h2>
+        <x-button.circle wire:click="openModal('create')" primary label="+" />
+    </section>
+
+    @include('livewire.institution.area.partials.table')
 </div>
