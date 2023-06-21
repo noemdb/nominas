12e9761f666a30dba0945cc9ef68f254cffdda96
noemdb <noemdb@gmail.com>
2023-06-21 09:13:41 -0400

    wip 2023-06-21_09:13:41

--- database/migrations/2023_06_21_122630_create_positions_table.php
+++ database/migrations/2023_06_21_122630_create_positions_table.php
@@ -14,7 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('positions', function (Blueprint $table) {
-            $table->id();
+            $table->id()->comment('Identificador único para cada posición');
+            $table->foreignId('area_id')->constrained()->comment('Área")');
+            $table->foreignId('rol_id')->constrained('roles')->comment('Rol');
+            $table->string('name')->comment('Nombre');
+            $table->text('description')->nullable()->comment('Descripción');
+            $table->date('start_date')->comment('Fecha de inicio');
+            $table->date('end_date')->nullable()->comment('Fecha de finalización');
             $table->timestamps();
         });
     }
