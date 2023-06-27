a47bdd051eb4963636fed597718807c540af08d4
jrafaaael <jrmd070102@gmail.com>
2023-06-27 12:00:18 -0400

    feat: list schedules

--- app/Http/Livewire/Institution/Schedule/IndexComponent.php
+++ app/Http/Livewire/Institution/Schedule/IndexComponent.php
@@ -2,12 +2,53 @@
 
 namespace App\Http\Livewire\Institution\Schedule;
 
+use App\Http\Livewire\Common\PaginateTrait;
+use App\Http\Livewire\Common\WithSortingTrait;
+use App\Models\Institution\Schedule;
 use Livewire\Component;
+use Livewire\WithPagination;
+use WireUi\Traits\Actions;
 
 class IndexComponent extends Component
 {
+    // use RolRules;
+    use Actions;
+    use WithSortingTrait;
+    use WithPagination;
+    use PaginateTrait;
+
+    public Schedule $schedule;
+    public $list_comment;
+    public $list_areas;
+    public $list_type;
+    public $showModal = false;
+    public $modeCreate = false;
+    public $modeEdit = false;
+    public $modeShow = false;
+
+    public function mount()
+    {
+        $this->schedule = new Schedule;
+        $this->list_comment = Schedule::COLUMN_COMMENTS;
+        $this->list_areas = Schedule::list_area();
+        $this->list_type = Schedule::list_type();
+    }
     public function render()
     {
-        return view('livewire.institution.schedule.index-component');
+        $search = $this->search;
+        $schedules = Schedule::select('schedules.*');
+
+        $schedules = (!empty($search)) ? $schedules->Where(
+            function ($query) use ($search) {
+                $query->orWhere('name', 'like', '%' . $search . '%');
+                $query->orWhere('description', 'like', '%' . $search . '%');
+            }
+        )
+            : $schedules;
+
+        $schedules = ($this->sortBy && $this->sortDirection) ? $schedules->orderBy($this->sortBy, $this->sortDirection) : $schedules;
+
+        $schedules = $schedules->paginate($this->paginate);
+        return view('livewire.institution.schedule.index-component', ['schedules' => $schedules]);
     }
 }
--- resources/views/livewire/institution/schedule/partials/table.blade.php
+++ resources/views/livewire/institution/schedule/partials/table.blade.php
@@ -1,44 +1,100 @@
-<div class="overflow-x-auto soft-scrollbar">
-<table class="min-w-full table-auto">
-    <thead>
-        <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-            <th class="py-3 px-6 text-left">Día</th>
-            <th class="py-3 px-6 text-left">Hora de entrada</th>
-            <th class="py-3 px-6 text-left">Hora de salida</th>
-            <th class="py-3 px-6 text-left">Horas trabajadas</th>
-        </tr>
-    </thead>
-    <tbody class="text-gray-600 text-sm font-light">
-        <tr class="border-b border-gray-200 hover:bg-gray-100">
-            <td class="py-3 px-6 text-left whitespace-nowrap">Lunes</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">8:00 am</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">5:00 pm</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">8</td>
-        </tr>
-        <tr class="border-b border-gray-200hover:bg-gray-100">
-            <td class="py-3 px-6 text-left whitespace-nowrap">Martes</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">9:00 am</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">6:00 pm</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">8</td>
-        </tr>
-        <tr class="border-b border-gray-200 hover:bg-gray-100">
-            <td class="py-3 px-6 text-left whitespace-nowrap">Miércoles</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">8:30 am</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">5:30 pm</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">8</td>
-        </tr>
-        <tr class="border-b border-gray-200 hover:bg-gray-100">
-            <td class="py-3 px-6 text-left whitespace-nowrap">Jueves</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">8:00 am</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">5:00 pm</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">8</td>
-        </tr>
-        <tr class="border-b border-gray-200 hover:bg-gray-100">
-            <td class="py-3 px-6 text-left whitespace-nowrap">Viernes</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">9:00 am</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">6:00 pm</td>
-            <td class="py-3 px-6 text-left whitespace-nowrap">8</td>
-        </tr>
-    </tbody>
-</table>
+@php
+    $class['index'] = 'hidden sm:table-cell';
+    $class['name'] = '';
+    $class['description'] = 'hidden md:table-cell';
+@endphp
+
+<div class="mb-4 flex justify-between flex-col gap-4 md:flex-row">
+    <div class="w-full md:w-3/4">
+        @php
+            $name = 'search';
+            $model = 'schedule.' . $name;
+        @endphp
+        <span wire:loading
+            class="text-black font-semibold fixed	 bottom-0 right-0 z-10 bg-white rounded border shadow mr-2 mb-2 dark:text-gray-100">
+            Cargando...
+        </span>
+        @php $label = "Nombre, descripción" @endphp
+        <x-input wire:model.debounce.500ms="{{ $name }}" icon="search" label="{{ $label }}"
+            placeholder="{{ $label }}">
+            <x-slot name="append">
+                <button class="absolute inset-y-0 right-0 flex items-center p-4 text-gray-600" wire:click="cleanSearch()">
+                    <x-icon name="x" class="w-4 h-4" />
+                </button>
+            </x-slot>
+        </x-input>
+    </div>
+    <div class="w-full md:w-1/5">
+        <x-select label="Registros por página" wire:model="paginate" placeholder="Páginas" :options="$paginate_list" />
+    </div>
+</div>
+
+<div class="overflow-x-auto">
+    <table class="mb-4 table-auto w-full text-left whitespace-no-wrap">
+        <thead>
+            <tr class="bg-gray-200 text-sm">
+                <th class="px-2 py-1 {{ $class['index'] ?? null }}">
+                    ID
+                </th>
+                <th class="px-2 py-1 {{ $class['name'] ?? null }}">
+                    <div class="flex justify-between">
+                        @php $name = 'name' @endphp
+                        <span>{{ $list_comment[$name] ?? '' }}</span>
+                        @if ($schedules->isNotEmpty())
+                            <x-elements.crud.sort-by field="{{ $name }}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
+                        @endif
+                    </div>
+                </th>
+                <th class="px-2 py-1 {{ $class['description'] ?? null }}">
+                    <div class="flex justify-between">
+                        @php $name = 'description' @endphp
+                        <span>{{ $list_comment[$name] ?? '' }}</span>
+                        @if ($schedules->isNotEmpty())
+                            <x-elements.crud.sort-by field="{{ $name }}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
+                        @endif
+                    </div>
+                </th>
+                <th class="px-2 py-1 {{ $class['action'] ?? null }}">
+                    Acción
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($schedules as $item)
+                <tr
+                    class="border-t text-xs text-gray-600 border-gray-200 {{ $loop->iteration % 2 == 0 ? 'bg-gray-100' : 'bg-white' }}">
+                    <td class="px-2 py-1 {{ $class['index'] ?? null }}">
+                        {{ $loop->iteration }}
+                    </td>
+                    <td class="px-2 py-1 {{ $class['name'] ?? null }}">
+                        {{ $item->name }}
+                        <div>
+                            <span class="text-xs text-gray-400">{{ $item->area->name ?? '' }}</span>
+                        </div>
+                    </td>
+                    <td class="px-2 py-1 {{ $class['description'] ?? null }}">{{ $item->description }}</td>
+                    <td class="px-2 py-1 {{ $class['action'] ?? null }}">
+                        <div class="flex items-center space-x-end">
+                            <x-button squared sm wire:click="show({{ $item->id }})" info icon="information-circle"
+                                class="px-4 rounded-l" />
+                            <x-button squared sm wire:click="edit({{ $item->id }})" warning icon="pencil-alt"
+                                class="px-4" />
+                            <x-button squared sm wire:click="deleteQuestion({{ $item->id }})" negative
+                                icon="x" class="px-4 rounded-r" />
+                        </div>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="4">
+                        No hay datos
+                    </td>
+                </tr>
+            @endforelse
+
+        </tbody>
+    </table>
+
+    {{ $schedules->links() }}
+
 </div>
