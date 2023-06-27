134c1005096a71685dce7631b7780410887ef446
jrafaaael <jrmd070102@gmail.com>
2023-06-27 11:31:57 -0400

    fix: save `id` instead of name

--- resources/views/livewire/institution/bank/form/fields.blade.php
+++ resources/views/livewire/institution/bank/form/fields.blade.php
@@ -6,7 +6,7 @@
             $comment = $list_comment[$name];
         @endphp
         <x-native-select label="{{ $comment }}" placeholder="Seleccione" :options="$list_institution"
-            wire:model.defer="{{ $model }}" />
+            wire:model.defer="{{ $model }}" option-key-value />
     </div>
 
     <div class="py-4">
@@ -55,7 +55,7 @@
             $comment = $list_comment[$name];
         @endphp
         <x-native-select label="{{ $comment }}" placeholder="Seleccione" :options="$list_account_type"
-            wire:model.defer="{{ $model }}" />
+            wire:model.defer="{{ $model }}" option-key-value />
     </div>
 
     <div class="py-4">
--- resources/views/livewire/institution/rol/form/fields.blade.php
+++ resources/views/livewire/institution/rol/form/fields.blade.php
@@ -6,7 +6,7 @@
             $comment = $list_comment[$name];
         @endphp
         <x-native-select label="{{ $comment }}" placeholder="Seleccione" :options="$list_areas"
-            wire:model.defer="{{ $model }}" />
+            wire:model.defer="{{ $model }}" option-key-value />
     </div>
 
     <div class="py-4">
