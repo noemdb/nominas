c3c104bc8c2912904c7b24dc6bc8e45cb54f3370
jrafaaael <jrmd070102@gmail.com>
2023-06-27 09:52:42 -0400

    fix: add missing fields

--- resources/views/livewire/institution/bank/form/fields.blade.php
+++ resources/views/livewire/institution/bank/form/fields.blade.php
@@ -78,6 +78,56 @@
             placeholder="{{ $comment }}" />
     </div>
 
+    <div class="py-4">
+        @php
+            $name = 'iban';
+            $model = 'bank.' . $name;
+            $comment = $list_comment[$name];
+        @endphp
+        <x-input wire:model.defer="{{ $model }}" label="{{ $comment }}"
+            placeholder="{{ $comment }}" />
+    </div>
+
+    <div class="py-4">
+        @php
+            $name = 'contact_person';
+            $model = 'bank.' . $name;
+            $comment = $list_comment[$name];
+        @endphp
+        <x-input wire:model.defer="{{ $model }}" label="{{ $comment }}"
+            placeholder="{{ $comment }}" />
+    </div>
+
+    <div class="py-4">
+        @php
+            $name = 'phone_number';
+            $model = 'bank.' . $name;
+            $comment = $list_comment[$name];
+        @endphp
+        <x-inputs.maskable wire:model.defer="{{ $model }}" label="{{ $comment }}" mask="(###) ###-####"
+            placeholder="{{ $comment }}" />
+    </div>
+
+    <div class="py-4">
+        @php
+            $name = 'email';
+            $model = 'bank.' . $name;
+            $comment = $list_comment[$name];
+        @endphp
+        <x-input wire:model.defer="{{ $model }}" label="{{ $comment }}" placeholder="{{ $comment }}"
+            suffix="@mail.com" />
+    </div>
+
+    <div class="py-4">
+        @php
+            $name = 'address';
+            $model = 'bank.' . $name;
+            $comment = $list_comment[$name];
+        @endphp
+        <x-textarea wire:model.defer="{{ $model }}" label="{{ $comment }}"
+            placeholder="{{ $comment }}" />
+    </div>
+
     <x-errors />
 
 </div>
