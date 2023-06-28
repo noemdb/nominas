21a69f4e5eedd12289cdc2ab9357a53ce4776511
jrafaaael <jrmd070102@gmail.com>
2023-06-28 09:15:42 -0400

    fix: add missing fields

--- resources/views/livewire/institution/schedule/form/fields.blade.php
+++ resources/views/livewire/institution/schedule/form/fields.blade.php
@@ -20,15 +20,56 @@
             wire:model.defer="{{ $model }}" option-key-value />
     </div>
 
-    {{-- <div class="py-4">
+    <div class="py-4">
         @php
-            $name = 'description';
+            $name = 'start_time';
+            $model = 'schedule.' . $name;
+            $comment = $list_comment[$name];
+        @endphp
+        <x-time-picker wire:model.defer="{{ $model }}" label="{{ $comment }}"
+            placeholder="{{ $comment }}" />
+    </div>
+
+    <div class="py-4">
+        @php
+            $name = 'end_time';
+            $model = 'schedule.' . $name;
+            $comment = $list_comment[$name];
+        @endphp
+        <x-time-picker wire:model.defer="{{ $model }}" label="{{ $comment }}"
+            placeholder="{{ $comment }}" />
+    </div>
+
+    <div class="py-4">
+        @php
+            $name = 'schedule_type';
+            $model = 'schedule.' . $name;
+            $comment = $list_comment[$name];
+        @endphp
+
+        <x-native-select label="{{ $comment }}" placeholder="Seleccione" :options="$list_type"
+            wire:model.defer="{{ $model }}" option-key-value />
+    </div>
+
+    <div class="py-4">
+        @php
+            $name = 'weekday';
+            $model = 'schedule.' . $name;
+            $comment = $list_comment[$name];
+        @endphp
+        <x-inputs.number wire:model.defer="{{ $model }}" label="{{ $comment }}"
+            placeholder="{{ $comment }}" />
+    </div>
+
+    <div class="py-4">
+        @php
+            $name = 'notes';
             $model = 'schedule.' . $name;
             $comment = $list_comment[$name];
         @endphp
         <x-textarea wire:model.defer="{{ $model }}" label="{{ $comment }}"
             placeholder="{{ $comment }}" />
-    </div> --}}
+    </div>
 
     <x-errors />
 
