348ba4c501da5508cadefbfbf517b1abafff54d6
jrafaaael <jrmd070102@gmail.com>
2023-06-30 12:20:27 -0400

    fix: make title less general

--- resources/views/livewire/employee/previous-work/index-component.blade.php
+++ resources/views/livewire/employee/previous-work/index-component.blade.php
@@ -3,7 +3,7 @@
     @include('livewire.employee.previous-work.modals.main')
 
     <section class="mb-4 flex justify-between items-center">
-        <h2 class="text-2xl font-bold">Trabajos registrados</h2>
+        <h2 class="text-2xl font-bold">Trabajos previos registrados</h2>
         <x-button.circle wire:click="openModal('create')" primary label="+" />
     </section>
 
