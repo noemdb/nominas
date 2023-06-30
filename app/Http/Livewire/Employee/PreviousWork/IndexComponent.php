bbcc95055c969a16ddcc5e9da733fcdd85cf005d
jrafaaael <jrmd070102@gmail.com>
2023-06-30 10:57:34 -0400

    feat: list previous_work

--- app/Http/Livewire/Employee/PreviousWork/IndexComponent.php
+++ app/Http/Livewire/Employee/PreviousWork/IndexComponent.php
@@ -2,12 +2,51 @@
 
 namespace App\Http\Livewire\Employee\PreviousWork;
 
+use App\Http\Livewire\Common\PaginateTrait;
+use App\Http\Livewire\Common\WithSortingTrait;
+use App\Models\Employee\PreviousWork;
 use Livewire\Component;
+use Livewire\WithPagination;
+use WireUi\Traits\Actions;
 
 class IndexComponent extends Component
 {
+    // use BankRules;
+    use Actions;
+    use WithSortingTrait;
+    use WithPagination;
+    use PaginateTrait;
+
+    public PreviousWork $previousWork;
+    public $list_comment;
+    public $showModal = false;
+    public $modeCreate = false;
+    public $modeEdit = false;
+    public $modeShow = false;
+
+
+    public function mount()
+    {
+        $this->previousWork = new PreviousWork;
+        $this->list_comment = PreviousWork::COLUMN_COMMENTS;
+    }
+
     public function render()
     {
-        return view('livewire.employee.previous-work.index-component');
+        $search = $this->search;
+        $previousWorks = PreviousWork::select('previous_works.*');
+
+        $previousWorks = (!empty($search)) ? $previousWorks->Where(
+            function ($query) use ($search) {
+                $query->orWhere('name', 'like', '%' . $search . '%');
+                $query->orWhere('description', 'like', '%' . $search . '%');
+            }
+        )
+            : $previousWorks;
+
+        $previousWorks = ($this->sortBy && $this->sortDirection) ? $previousWorks->orderBy($this->sortBy, $this->sortDirection) : $previousWorks;
+
+        $previousWorks = $previousWorks->paginate($this->paginate);
+        return view('livewire.employee.previous-work.index-component', ['previousWorks' => $previousWorks]);
     }
 }
---
+++ resources/views/livewire/employee/previous-work/partials/table.blade.php
@@ -0,0 +1,114 @@
+@php
+    $class['index'] = 'hidden sm:table-cell';
+    $class['name'] = '';
+    $class['account_number'] = 'hidden md:table-cell';
+    $class['account_type'] = 'hidden lg:table-cell';
+    $class['action'] = '';
+@endphp
+
+<div class="mb-4 flex justify-between flex-col gap-4 md:flex-row">
+    <div class="w-full md:w-3/4">
+        @php
+            $name = 'search';
+            $model = 'previousWorks.' . $name;
+        @endphp
+        <span wire:loading
+            class="text-black font-semibold fixed	 bottom-0 right-0 z-10 bg-white rounded border shadow mr-2 mb-2 dark:text-gray-100">
+            Cargando...
+        </span>
+        @php $label = "Nombre, número de cuenta, tipo de cuenta" @endphp
+        <x-input wire:model.debounce.500ms="{{ $name }}" icon="search" label="{{ $label }}"
+            placeholder="{{ $label }}">
+            <x-slot name="append">
+                <button class="absolute inset-y-0 right-0 flex items-center p-4 text-gray-600" wire:click="cleanSearch()">
+                    <x-icon name="x" class="w-4 h-4" />
+                </button>
+            </x-slot>
+        </x-input>
+    </div>
+    <div class="w-full md:w-1/5">
+        <x-select label="Registros por página" wire:model="paginate" placeholder="Páginas" :options="$paginate_list" />
+    </div>
+</div>
+
+<div class="overflow-x-auto">
+    <table class="mb-4 table-auto w-full text-left whitespace-no-wrap">
+        <thead>
+            <tr class="bg-gray-200 text-sm">
+                <th class="px-2 py-1 {{ $class['index'] ?? null }}">
+                    ID
+                </th>
+                <th class="px-2 py-1 {{ $class['name'] ?? null }}">
+                    <div class="flex justify-between">
+                        @php $name = 'name' @endphp
+                        <span>{{ $list_comment[$name] ?? '' }}</span>
+                        @if ($previousWorks->isNotEmpty())
+                            <x-elements.crud.sort-by field="{{ $name }}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
+                        @endif
+                    </div>
+                </th>
+                <th class="px-2 py-1 {{ $class['account_number'] ?? null }}">
+                    <div class="flex justify-between">
+                        @php $name = 'account_number' @endphp
+                        <span>{{ $list_comment[$name] ?? '' }}</span>
+                        @if ($previousWorks->isNotEmpty())
+                            <x-elements.crud.sort-by field="{{ $name }}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
+                        @endif
+                    </div>
+                </th>
+                <th class="px-2 py-1 {{ $class['account_type'] ?? null }}">
+                    <div class="flex justify-between">
+                        @php $name = 'account_type' @endphp
+                        <span>{{ $list_comment[$name] ?? '' }}</span>
+                        @if ($previousWorks->isNotEmpty())
+                            <x-elements.crud.sort-by field="{{ $name }}" :sortBy="$sortBy" :sortDirection="$sortDirection" />
+                        @endif
+                    </div>
+                </th>
+                <th class="px-2 py-1 {{ $class['action'] ?? null }}">
+                    Acción
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($previousWorks as $item)
+                <tr
+                    class="border-t text-xs text-gray-600 border-gray-200 {{ $loop->iteration % 2 == 0 ? 'bg-gray-100' : 'bg-white' }}">
+                    <td class="px-2 py-1 {{ $class['index'] ?? null }}">
+                        {{ $loop->iteration }}
+                    </td>
+                    <td class="px-2 py-1 {{ $class['name'] ?? null }}">
+                        {{ $item->name }}
+                        <div>
+                            <span class="text-xs text-gray-400">{{ $item->institution->name ?? '' }}</span>
+                        </div>
+                    </td>
+                    <td class="px-2 py-1 {{ $class['account_number'] ?? null }}">{{ $item->account_number }}</td>
+                    <td class="px-2 py-1 {{ $class['account_type'] ?? null }}">
+                        {{ $item->account_type }}
+                    </td>
+                    <td class="px-2 py-1 {{ $class['action'] ?? null }}">
+                        <div class="flex items-center space-x-end">
+                            <x-button squared sm wire:click="show({{ $item->id }})" info icon="information-circle"
+                                class="px-4 rounded-l" />
+                            <x-button squared sm wire:click="edit({{ $item->id }})" warning icon="pencil-alt"
+                                class="px-4" />
+                            <x-button squared sm wire:click="deleteQuestion({{ $item->id }})" negative
+                                icon="x" class="px-4 rounded-r" />
+                        </div>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="4">
+                        No hay datos
+                    </td>
+                </tr>
+            @endforelse
+
+        </tbody>
+    </table>
+
+    {{ $previousWorks->links() }}
+
+</div>
