4fd5666f5a9f725ca2e25bbd465903ae10537d74
jrafaaael <jrmd070102@gmail.com>
2023-07-04 12:11:09 -0400

    fix: set same styles of others inputs in `math-field`

--- resources/views/livewire/formulation/home/form/fields.blade.php
+++ resources/views/livewire/formulation/home/form/fields.blade.php
@@ -60,7 +60,9 @@
             $comment = $list_comment[$name];
         @endphp
         <label class="text-sm">{{ $comment }}</label>
-        <math-field wire:model.defer="{{ $model }}" id="latex"></math-field>
+        <math-field wire:model.defer="{{ $model }}" id="latex"
+            class="placeholder-secondary-400 dark:bg-secondary-800 dark:text-secondary-400 dark:placeholder-secondary-500 border border-secondary-300 focus:ring-primary-500 focus:border-primary-500 dark:border-secondary-600 form-input block w-full sm:text-sm rounded-md transition ease-in-out duration-100 focus:outline-none shadow-sm">
+        </math-field>
     </div>
 
     <x-errors />
