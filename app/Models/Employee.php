e65eaea68524736cbe34ce0ae202dbbaf96547c1
jrafaaael <jrmd070102@gmail.com>
2023-07-07 09:31:44 -0400

    idk

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -28,6 +28,22 @@ class Employee extends Model
 
     const VARS = ['a' => 'a', 'b' => 'b', 'c' => 'c', 'd' => 'd'];
 
+    public function a () {
+        return 5;
+    }
+
+    public function b () {
+        return 6;
+    }
+
+    public function c () {
+        return 7;
+    }
+
+    public function d () {
+        return 8;
+    }
+
     public function getInstitutionNameAttribute()
     {
         return ($this->institution) ? $this->institution->name : null ;
